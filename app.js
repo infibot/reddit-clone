378b67546cb1afa96c8a5b9d33af5daf547d6c0b
Lewis carhart <lewiscarhart@tucasi.com>
2016-11-18 20:29:29 +0000

    Initial commit of app.js & index.html

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+'$scope$',
+function($scope$) {
+	$scope.test = 'Hello world!';
+
+	$scope.posts = [
+	'Post 1',
+	'Post 2',
+	'Post 3',
+	'Post 4',
+	'Post 5'
+	];	
+
+}]);
