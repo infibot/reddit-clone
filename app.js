25199d0daace35429db4a596b6ca046aa870ba1c
Lewis carhart <lewiscarhart@tucasi.com>
2016-11-29 19:20:40 +0000

    initialization of app

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
-'$scope$',
-function($scope$) {
-	$scope.test = 'Hello world!';
-
-	$scope.posts = [
-	'Post 1',
-	'Post 2',
-	'Post 3',
-	'Post 4',
-	'Post 5'
-	];	
-
-}]);
+	'$scope$',
+	function($scope) {
+		$scope.test = 'Hello world!';
+	}]);
